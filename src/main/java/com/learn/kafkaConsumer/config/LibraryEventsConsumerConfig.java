8aaa37406a78c666db4b722c377c3b5a84b65c60
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-07-31 23:08:41 +0530

    create kafkaListener, closes #1

---
+++ src/main/java/com/learn/kafkaConsumer/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,11 @@
+package com.learn.kafkaConsumer.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+@Slf4j
+public class LibraryEventsConsumerConfig {
+}
